bb78e9bc60a6f2f4b10e42cbfd561212ba80391d
Trung Nguyen <ndactrung@gmail.com>
2021-08-05 11:59:39 -0500

    Added changes to FixGPU to allow newton pair to be on

--- src/GPU/pair_lj_cut_gpu.cpp
+++ src/GPU/pair_lj_cut_gpu.cpp
@@ -134,10 +134,10 @@ void PairLJCutGPU::compute(int eflag, int vflag)
 void PairLJCutGPU::init_style()
 {
   cut_respa = nullptr;
-
+/*
   if (force->newton_pair)
     error->all(FLERR,"Pair style lj/cut/gpu requires newton pair off");
-
+*/
   // Repeat cutsq calculation because done after call to init_style
   double maxcut = -1.0;
   double cut;
