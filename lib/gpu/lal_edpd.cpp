5e828361cd69483a42ae08447dd577a59558d8ec
Trung Nguyen <ndactrung@gmail.com>
2023-09-16 16:32:59 -0500

    Started to support pair edpd/gpu

---
+++ lib/gpu/lal_edpd.cpp
@@ -0,0 +1,206 @@
+/***************************************************************************
+                                   eEDPD.cpp
+                             -------------------
+                            Trung Dac Nguyen (U Chicago)
+
+  Class for acceleration of the edpd pair style.
+
+ __________________________________________________________________________
+    This file is part of the LAMMPS Accelerator Library (LAMMPS_AL)
+ __________________________________________________________________________
+
+    begin                : September 2023
+    email                : ndactrung@gmail.com
+ ***************************************************************************/
+
+#if defined(USE_OPENCL)
+#include "edpd_cl.h"
+#elif defined(USE_CUDART)
+const char *edpd=0;
+#else
+#include "edpd_cubin.h"
+#endif
+
+#include "lal_edpd.h"
+#include <cassert>
+namespace LAMMPS_AL {
+#define EDPDT EDPD<numtyp, acctyp>
+
+extern Device<PRECISION,ACC_PRECISION> device;
+
+template <class numtyp, class acctyp>
+EDPDT::EDPD() : BaseDPD<numtyp,acctyp>(), _allocated(false) {
+  _nmax = 0;
+}
+
+template <class numtyp, class acctyp>
+EDPDT::~EDPD() {
+  clear();
+}
+
+template <class numtyp, class acctyp>
+int EDPDT::bytes_per_atom(const int max_nbors) const {
+  return this->bytes_per_atom_atomic(max_nbors);
+}
+
+template <class numtyp, class acctyp>
+int EDPDT::init(const int ntypes,
+               double **host_cutsq, double **host_a0,
+               double **host_gamma, double **host_sigma,
+               double **host_cut, double **host_power,
+               double **host_kappa, double **host_powerT,
+               double **host_cutT, double *host_special_lj,
+               const bool tstat_only,
+               const int nlocal, const int nall,
+               const int max_nbors, const int maxspecial,
+               const double cell_size,
+               const double gpu_split, FILE *_screen) {
+  const int max_shared_types=this->device->max_shared_types();
+
+  int onetype=0;
+  #ifdef USE_OPENCL
+  if (maxspecial==0)
+    for (int i=1; i<ntypes; i++)
+      for (int j=i; j<ntypes; j++)
+        if (host_cutsq[i][j]>0) {
+          if (onetype>0)
+            onetype=-1;
+          else if (onetype==0)
+            onetype=i*max_shared_types+j;
+        }
+  if (onetype<0) onetype=0;
+  #endif
+
+  int success;
+  success=this->init_atomic(nlocal,nall,max_nbors,maxspecial,cell_size,
+                            gpu_split,_screen,edpd,"k_edpd",onetype);
+  if (success!=0)
+    return success;
+
+  // If atom type constants fit in shared memory use fast kernel
+  int lj_types=ntypes;
+  shared_types=false;
+  if (lj_types<=max_shared_types && this->_block_size>=max_shared_types) {
+    lj_types=max_shared_types;
+    shared_types=true;
+  }
+  _lj_types=lj_types;
+
+  // Allocate a host write buffer for data initialization
+  UCL_H_Vec<numtyp> host_write(lj_types*lj_types*32,*(this->ucl_device),
+                               UCL_WRITE_ONLY);
+
+  for (int i=0; i<lj_types*lj_types; i++)
+    host_write[i]=0.0;
+
+  coeff.alloc(lj_types*lj_types,*(this->ucl_device),UCL_READ_ONLY);
+  this->atom->type_pack4(ntypes,lj_types,coeff,host_write,host_a0,host_gamma,
+                         host_sigma,host_cut);
+
+  coeff2.alloc(lj_types*lj_types,*(this->ucl_device),UCL_READ_ONLY);
+  this->atom->type_pack4(ntypes,lj_types,coeff,host_write,host_power,host_kappa,
+                         host_powerT,host_cutT);
+
+  UCL_H_Vec<numtyp> host_rsq(lj_types*lj_types,*(this->ucl_device),
+                             UCL_WRITE_ONLY);
+  cutsq.alloc(lj_types*lj_types,*(this->ucl_device),UCL_READ_ONLY);
+  this->atom->type_pack1(ntypes,lj_types,cutsq,host_rsq,host_cutsq);
+
+  double special_sqrt[4];
+  special_sqrt[0] = sqrt(host_special_lj[0]);
+  special_sqrt[1] = sqrt(host_special_lj[1]);
+  special_sqrt[2] = sqrt(host_special_lj[2]);
+  special_sqrt[3] = sqrt(host_special_lj[3]);
+
+  UCL_H_Vec<double> dview;
+  sp_lj.alloc(4,*(this->ucl_device),UCL_READ_ONLY);
+  dview.view(host_special_lj,4,*(this->ucl_device));
+  ucl_copy(sp_lj,dview,false);
+  sp_sqrt.alloc(4,*(this->ucl_device),UCL_READ_ONLY);
+  dview.view(special_sqrt,4,*(this->ucl_device));
+  ucl_copy(sp_sqrt,dview,false);
+
+  _tstat_only = 0;
+  if (tstat_only) _tstat_only=1;
+
+  if (_nmax < nall) {
+    _nmax=static_cast<int>(static_cast<double>(nall)*1.10);
+    Q.alloc(_nmax,*(this->ucl_device),UCL_READ_WRITE,UCL_READ_WRITE);
+  }
+
+  _allocated=true;
+  this->_max_bytes=coeff.row_bytes()+cutsq.row_bytes()+sp_lj.row_bytes()+sp_sqrt.row_bytes();
+  return 0;
+}
+
+template <class numtyp, class acctyp>
+void EDPDT::clear() {
+  if (!_allocated)
+    return;
+  _allocated=false;
+
+  coeff.clear();
+  cutsq.clear();
+  sp_lj.clear();
+  sp_sqrt.clear();
+  this->clear_atomic();
+}
+
+template <class numtyp, class acctyp>
+double EDPDT::host_memory_usage() const {
+  return this->host_memory_usage_atomic()+sizeof(EDPD<numtyp,acctyp>);
+}
+
+// ---------------------------------------------------------------------------
+// Calculate energies, forces, and torques
+// ---------------------------------------------------------------------------
+template <class numtyp, class acctyp>
+int EDPDT::loop(const int eflag, const int vflag) {
+  // Compute the block size and grid size to keep all cores busy
+  const int BX=this->block_size();
+  int GX=static_cast<int>(ceil(static_cast<double>(this->ans->inum())/
+                               (BX/this->_threads_per_atom)));
+
+  int nall = this->atom->nall();
+  if (_nmax < nall) {
+    _nmax = nall;
+    Q.resize(_nmax);
+  }
+
+  int ainum=this->ans->inum();
+  int nbor_pitch=this->nbor->nbor_pitch();
+  this->time_pair.start();
+  if (shared_types) {
+    this->k_pair_sel->set_size(GX,BX);
+    this->k_pair_sel->run(&this->atom->x, &this->atom->extra, &coeff, &coeff2,
+                          &sp_lj, &sp_sqrt, &this->nbor->dev_nbor, &this->_nbor_data->begin(),
+                          &this->ans->force, &this->ans->engv, &Q, &eflag,
+                          &vflag, &ainum, &nbor_pitch, &this->atom->v, &cutsq,
+                          &this->_dtinvsqrt, &this->_seed, &this->_timestep,
+                          &this->_tstat_only, &this->_threads_per_atom);
+  } else {
+    this->k_pair.set_size(GX,BX);
+    this->k_pair.run(&this->atom->x, &this->atom->extra, &coeff, &coeff2,
+                     &_lj_types, &sp_lj, &sp_sqrt,
+                     &this->nbor->dev_nbor, &this->_nbor_data->begin(),
+                     &this->ans->force, &this->ans->engv, &Q, &eflag, &vflag,
+                     &ainum, &nbor_pitch, &this->atom->v, &cutsq, &this->_dtinvsqrt,
+                     &this->_seed, &this->_timestep, &this->_tstat_only,
+                     &this->_threads_per_atom);
+  }
+  this->time_pair.stop();
+  return GX;
+}
+
+template <class numtyp, class acctyp>
+void EDPDT::update_coeff(int ntypes, double **host_a0, double **host_gamma,
+                        double **host_sigma, double **host_cut)
+{
+  UCL_H_Vec<numtyp> host_write(_lj_types*_lj_types*32,*(this->ucl_device),
+                               UCL_WRITE_ONLY);
+  this->atom->type_pack4(ntypes,_lj_types,coeff,host_write,host_a0,host_gamma,
+                         host_sigma,host_cut);
+}
+
+template class EDPD<PRECISION,ACC_PRECISION>;
+}
---
+++ lib/gpu/lal_edpd.h
@@ -0,0 +1,94 @@
+/***************************************************************************
+                                 edpd.h
+                             -------------------
+                            Trung Dac Nguyen (U Chicago)
+
+  Class for acceleration of the dpd pair style.
+
+ __________________________________________________________________________
+    This file is part of the LAMMPS Accelerator Library (LAMMPS_AL)
+ __________________________________________________________________________
+
+    begin                : September 2023
+    email                : ndactrung@gmail.com
+ ***************************************************************************/
+
+#ifndef LAL_DPD_H
+#define LAL_DPD_H
+
+#include "lal_base_dpd.h"
+
+namespace LAMMPS_AL {
+
+template <class numtyp, class acctyp>
+class EDPD : public BaseDPD<numtyp, acctyp> {
+ public:
+  EDPD();
+  ~EDPD();
+
+  /// Clear any previous data and set up for a new LAMMPS run
+  /** \param max_nbors initial number of rows in the neighbor matrix
+    * \param cell_size cutoff + skin
+    * \param gpu_split fraction of particles handled by device
+    *
+    * Returns:
+    * -  0 if successful
+    * - -1 if fix gpu not found
+    * - -3 if there is an out of memory error
+    * - -4 if the GPU library was not compiled for GPU
+    * - -5 Double precision is not supported on card **/
+  int init(const int ntypes, double **host_cutsq, double **host_a0,
+           double **host_gamma, double **host_sigma, double **host_cut,
+           double **host_power, double **host_kappa, double **host_powerT,
+           double **host_cutT, double *host_special_lj, bool tstat_only,
+           const int nlocal, const int nall, const int max_nbors,
+           const int maxspecial, const double cell_size, const double gpu_split,
+           FILE *screen);
+
+  /// Clear all host and device data
+  /** \note This is called at the beginning of the init() routine **/
+  void clear();
+
+  /// Returns memory usage on device per atom
+  int bytes_per_atom(const int max_nbors) const;
+
+  /// Total host memory used by library for pair style
+  double host_memory_usage() const;
+
+  /// Update coeff if needed (tstat only)
+  void update_coeff(int ntypes, double **host_a0, double **host_gamma,
+                    double **host_sigma, double **host_cut);
+
+  // --------------------------- TYPE DATA --------------------------
+
+  /// coeff.x = a0, coeff.y = gamma, coeff.z = sigma, coeff.w = cut
+  UCL_D_Vec<numtyp4> coeff;
+  /// coeff2.x = power, coeff2.y = kappa, coeff2.z = powerT, coeff2.w = cutT
+  UCL_D_Vec<numtyp4> coeff2;
+
+  UCL_D_Vec<numtyp> cutsq;
+
+  /// Special LJ values
+  UCL_D_Vec<numtyp> sp_lj, sp_sqrt;
+
+  /// If atom type constants fit in shared memory, use fast kernels
+  bool shared_types;
+
+  /// Number of atom types
+  int _lj_types;
+
+  /// Only used for thermostat
+  int _tstat_only;
+
+  /// Per-atom arrays
+  UCL_Vector<acctyp,acctyp> Q;
+  int _nmax;
+
+ private:
+  bool _allocated;
+  int loop(const int eflag, const int vflag);
+};
+
+}
+
+#endif
